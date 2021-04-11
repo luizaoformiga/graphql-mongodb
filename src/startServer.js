d0d75aef02499415eecc850ebee24c7dbd8b0749
luizaoformiga <pereiralimaluizpaulo7@gmail.com>
2021-04-11 13:56:57 -0300

    part 3

--- src/startServer.js
+++ src/startServer.js
@@ -1,13 +1,8 @@
 import { ApolloServer } from 'apollo-server';
-import mongoose from 'mongoose';
+import database from './mongodb/database';
 
 export default function startServer({ typeDefs, resolvers }) {
-    mongoose.connect('mongodb://localhost:27017/graphql-node', {
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useFindAndModify: false
-,    })
+    database.connect();    
     
     const server = new ApolloServer({ typeDefs, resolvers });
     server.listen().then(({ url }) => console.log(`Server running to port: http://localhost:${url}`));
